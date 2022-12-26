0094fd949f497b898d8d4cfefea31fdde579bae2
evyatar cohen <evycoh3.com>
2022-12-26 12:56:15 +0200

    up

--- src/main/java/Excel/WriteToExcel.java
+++ src/main/java/Excel/WriteToExcel.java
@@ -1,2 +1,10 @@
-package Excel;public class WriteToExcel {
+package Excel;
+
+import java.io.FileInputStream;
+
+public class WriteToExcel {
+    public static void main(String[] args) {
+        FileInputStream fis=new FileInputStream(System.getProperty("user.dir")+"/src/main/resources/Book1.xlsx");
+
+    }
 }
